325640142dcf5b7f5e4cea9e6dcb709be100b62a
Terence625 <terencetianterence@gmail.com>
2022-06-10 19:20:35 +1000

    [Terence] add cors in expressjs and add createClientForm component

--- backend/src/index.js
+++ backend/src/index.js
@@ -26,6 +26,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use((req, res, next) => {
   console.log(`${req.method} - ${req.url}`);
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 app.use(passport.initialize());
--- frontend/src/components/CreateClientForm/CreateClientForm.jsx
+++ frontend/src/components/CreateClientForm/CreateClientForm.jsx
@@ -1,40 +1,68 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 export const CreateClientForm = () => {
   const [formText, setFormText] = useState({ name: "", phone: "", email: "" });
+  const [clientId, setClientId] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  const submitData = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    try {
+      const result = await axios({
+        method: "post",
+        url: "http://localhost:3001/createClient",
+        data: {
+          name: formText.name,
+          phone: formText.phone,
+          email: formText.email,
+        },
+      });
+      setClientId(result.data.clientId);
+    } catch (error) {
+      setIsError(true);
+    }
+    setIsLoading(false);
+  };
 
   return (
-    <form
-      onSubmit={(e) => {
-        alert(formText.name + " " + formText.phone + " " + formText.email);
-        e.preventDefault();
-      }}
-    >
-      <label>
-        Name:
-        <input
-          type="text"
-          value={formText.name}
-          onChange={(e) => setFormText({ ...formText, name: e.target.value })}
-        />
-      </label>
-      <label>
-        Phone number:
-        <input
-          type="text"
-          value={formText.phone}
-          onChange={(e) => setFormText({ ...formText, phone: e.target.value })}
-        />
-      </label>
-      <label>
-        E-mail:
-        <input
-          type="text"
-          value={formText.email}
-          onChange={(e) => setFormText({ ...formText, email: e.target.value })}
-        />
-      </label>
-      <input type="submit" value="Submit" />
-    </form>
+    <div>
+      <form onSubmit={submitData}>
+        <label>
+          Name:
+          <input
+            type="text"
+            value={formText.name}
+            onChange={(e) => setFormText({ ...formText, name: e.target.value })}
+          />
+        </label>
+        <label>
+          Phone number:
+          <input
+            type="text"
+            value={formText.phone}
+            onChange={(e) =>
+              setFormText({ ...formText, phone: e.target.value })
+            }
+          />
+        </label>
+        <label>
+          E-mail:
+          <input
+            type="text"
+            value={formText.email}
+            onChange={(e) =>
+              setFormText({ ...formText, email: e.target.value })
+            }
+          />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+      {isLoading && <div>Loading ...</div>}
+      {isError && <div>Something went wrong ...</div>}
+      {clientId !== "" && <div>Create successfully: {`${clientId}`}</div>}
+    </div>
   );
 };
