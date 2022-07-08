c2ba1ef56be9e855d4708d0faed0c5bacce8c097
Terence625 <terencetianterence@gmail.com>
2022-07-08 18:15:15 +1000

    [Terence] transfer ConfirmDialog to tsx

--- frontend/src/pages/common/ConfirmDialog.tsx
+++ frontend/src/pages/common/ConfirmDialog.tsx
@@ -1,21 +1,31 @@
 import React from "react";
 import "./ConfirmDialog.css";
 
+interface IConfirmDialog {
+  text: string;
+  confirmButtonText: string;
+  cancelButtonText: string;
+  handleConfirm: () => void;
+  handleCancel: (
+    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>
+  ) => void;
+}
+
 const ConfirmDialog = ({
   text,
   confirmButtonText,
   cancelButtonText,
   handleConfirm,
   handleCancel,
-}) => {
+}: IConfirmDialog) => {
   return (
     <React.Fragment>
       <div className="greyCover"></div>
       <div className="ConfirmDialog">
         <p>{text}</p>
         <div className="buttonGroup">
-          <button onClick={handleConfirm}>{confirmButtonText}</button>
-          <button onClick={handleCancel}>{cancelButtonText}</button>
+          <button onClick={() => handleConfirm}>{confirmButtonText}</button>
+          <button onClick={() => handleCancel}>{cancelButtonText}</button>
         </div>
       </div>
     </React.Fragment>
