017c7012a33d9d8a630413016829d881429e7c34
Terence625 <terencetianterence@gmail.com>
2022-07-20 22:41:36 +1000

    [Terence] apply useRequest custom hook to SearchPersonPage component

--- frontend/src/pages/search-person-page/SearchPersonPage.tsx
+++ frontend/src/pages/search-person-page/SearchPersonPage.tsx
@@ -1,44 +1,40 @@
-import axios from "axios";
 import PageContainer from "../common/PageContainer";
 import React, { useState } from "react";
 import SearchBar from "./components/SearchBar";
 import ResultList from "./components/ResultList";
 import { Link } from "react-router-dom";
+import useRequest from "../hooks/useRequest";
 
 const SearchPersonPage = () => {
   const [searchValue, setSearchValue] = useState("");
-  const [searchResult, setSearchResult] = useState();
   const [currentPage, setCurrentPage] = useState(1);
-  const [isLoading, setIsLoading] = useState(false);
-  const [isError, setIsError] = useState(false);
-
-  const receiveData = async () => {
-    setIsLoading(true);
-    setIsError(false);
-    try {
-      const result = await axios({
-        method: "get",
-        url: `/searchPerson?name=${searchValue}`,
-      });
-      setSearchResult(result.data.personList);
-    } catch (error) {
-      setIsError(true);
+  const { isError, isLoading, request, response } = useRequest<
+    string,
+    {
+      personList: {
+        person_id: number;
+        name: string;
+      }[];
     }
-    setIsLoading(false);
-    setCurrentPage(1);
-  };
+  >({
+    method: "get",
+    url: `/searchPerson?name=${searchValue}`,
+  });
 
   return (
     <PageContainer isLoading={isLoading} isError={isError}>
       <SearchBar
-        receiveData={receiveData}
+        receiveData={() => {
+          request();
+          setCurrentPage(1);
+        }}
         searchValue={searchValue}
         onSearchValueChange={(value) => setSearchValue(value)}
       />
       <Link to={"/createPerson"}>Create New Person</Link>
-      {searchResult && (
+      {response && (
         <ResultList
-          searchResult={searchResult}
+          searchResult={response.personList}
           currentPage={currentPage}
           onSelectPage={(value) => setCurrentPage(value)}
         />
