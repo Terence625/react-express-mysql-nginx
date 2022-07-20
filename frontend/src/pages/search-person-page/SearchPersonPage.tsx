21bd8d435aed7ea7308aceef28a14e8738924059
Terence625 <terencetianterence@gmail.com>
2022-07-20 17:23:48 +1000

    Revert "[Terence] add custome hook useRequest and apply to searchPersonPage"
    This reverts commit 3d11c05e1e7baff915f2f4e84aa830373a9e58d8.

--- frontend/src/pages/search-person-page/SearchPersonPage.tsx
+++ frontend/src/pages/search-person-page/SearchPersonPage.tsx
@@ -1,37 +1,44 @@
+import axios from "axios";
 import PageContainer from "../common/PageContainer";
 import React, { useState } from "react";
 import SearchBar from "./components/SearchBar";
 import ResultList from "./components/ResultList";
 import { Link } from "react-router-dom";
-import useRequest from "../../hooks/useRequest";
 
 const SearchPersonPage = () => {
   const [searchValue, setSearchValue] = useState("");
-  const [url, setUrl] = useState("/searchPerson?name=");
+  const [searchResult, setSearchResult] = useState();
   const [currentPage, setCurrentPage] = useState(1);
-  const { isError, isLoading, response } = useRequest<{
-    personList: {
-      person_id: number;
-      name: string;
-      phone: string;
-      email: string;
-    }[];
-  }>({
-    method: "get",
-    url,
-  });
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  const receiveData = async () => {
+    setIsLoading(true);
+    setIsError(false);
+    try {
+      const result = await axios({
+        method: "get",
+        url: `/searchPerson?name=${searchValue}`,
+      });
+      setSearchResult(result.data.personList);
+    } catch (error) {
+      setIsError(true);
+    }
+    setIsLoading(false);
+    setCurrentPage(1);
+  };
 
   return (
     <PageContainer isLoading={isLoading} isError={isError}>
       <SearchBar
-        search={() => setUrl(`searchPerson?name=${searchValue}`)}
+        receiveData={receiveData}
         searchValue={searchValue}
         onSearchValueChange={(value) => setSearchValue(value)}
       />
       <Link to={"/createPerson"}>Create New Person</Link>
-      {response && response.personList && (
+      {searchResult && (
         <ResultList
-          searchResult={response.personList}
+          searchResult={searchResult}
           currentPage={currentPage}
           onSelectPage={(value) => setCurrentPage(value)}
         />
--- frontend/src/pages/search-person-page/components/SearchBar.tsx
+++ frontend/src/pages/search-person-page/components/SearchBar.tsx
@@ -2,13 +2,13 @@ import React from "react";
 import "./SearchBar.css";
 
 interface ISearchBarProps {
-  search: () => void;
+  receiveData: () => void;
   searchValue: string;
   onSearchValueChange: (vaue: string) => void;
 }
 
 const SearchBar = ({
-  search,
+  receiveData,
   searchValue,
   onSearchValueChange,
 }: ISearchBarProps) => {
@@ -18,7 +18,7 @@ const SearchBar = ({
       onSubmit={(e) => {
         e.preventDefault();
         if (!searchValue.replace(/\s/g, "")) return;
-        search();
+        receiveData();
       }}
     >
       <input
